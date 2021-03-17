9da669a3569d733b51cf4d1b7aaf55883898345a
Gregmar Cruz D.O <gregr1201@gmail.com>
2021-03-16 22:30:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JymTheAppV3
+ 
---
+++ src/screens/Home/index.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { StyleSheet, ImageBackground, Text, View, TouchableOpacity } from 'react-native'
+
+
+const HomeScreen = (props) => {
+    return (
+        <View style={styles.homeContainer}>
+            <ImageBackground 
+                source={require('../../../assets/images/jymWelcome.jpeg')} 
+                style={styles.backgroundImage}>
+                    <Text style={styles.title}>Jym The App</Text>
+            </ImageBackground>
+            <View style={styles.buttonContainer} >
+                <TouchableOpacity style={styles.button} onPress={() => console.warn('Button was pressed')}>
+                    <Text style={styles.buttonText}>Find a Jym Buddy</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer} >
+                <TouchableOpacity style={styles.button} onPress={() => console.warn('Button was pressed')}>
+                    <Text style={styles.buttonText}>Become a Jym Trainer</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'cover',
+    },
+    homeContainer : {
+        height: '100%',
+        backgroundColor: 'lightgrey'
+    },
+    title : {
+        fontSize: 45,
+        fontWeight: 'bold',
+        color: 'white',
+        width: '60%',
+        textAlign: 'center',
+        position : 'relative',
+        top : '10%',
+        right : '5%'
+    },
+    buttonContainer : {
+        marginTop: 70,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button : {
+        width: 250,
+        height: 50,
+        backgroundColor: 'purple',
+        borderColor: 'white',
+        borderWidth: 2,
+        borderRadius: 30,
+        borderStyle: 'solid',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+    },
+    buttonText : {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
+
+
